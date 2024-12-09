f2aaf9d6651a60fdf61b5be09ea981f601f0447e
Ha Cao <8521623+shanonhaliwell@users.noreply.github.com>
2024-12-09 17:34:23 +0700

    Chapter 12 ex2 first

---
+++ Chapter12/ch12_ex2/src/test/java/springstarthere/ch12_ex2/Ch12Ex2ApplicationTests.java
@@ -0,0 +1,13 @@
+package springstarthere.ch12_ex2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch12Ex2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
